ac50ed01418c4b537d9c0dd333593f42973cd25e
Thomas Bamelis <thomas@bamelis.dev>
2023-02-27 23:56:41 +0100

    range_cfg start + todos

--- rust/kernel/regmap.rs
+++ rust/kernel/regmap.rs
@@ -5,6 +5,18 @@
 //!
 //! C header: [`include/linux/spi/spi.h`](../../../../include/linux/spi/spi.h)
 
+/*
+    TODO: make impl from for i2c/spi/ struct. Put TODO there to make it consume the i2c_device etc.. from other pull requests.
+    in the case of the latter, you won't be able to use the trait because it can't get the regmap?
+    Or make wrapper struct that will work in both cases?
+    
+    TODO: figure out what types to give config fields and which to include or not.
+    unimplemented ones just become None in RegmapConfig
+    
+
+    TODO: much later, implement dev managened one and allow for configs which require dev argument
+ */
+
 use crate::c_str;
 use crate::bindings;
 use crate::error::{code::*, Error, Result, to_result};
@@ -13,11 +25,38 @@ use alloc::boxed::Box;
 use core::pin::Pin;
 use core::convert::{From, Into};
 
+/**
+Configuration for indirectly accessed or paged registers.
+
+Registers, mapped to this virtual range, are accessed in two steps:
+    1. page selector register update;
+    2. access through data windowregisters.
+ */
+pub struct RegmapRangeConfig {
+    /// Descriptive name for diagnostics
+    name: &'static CStr,
+    /// Address of the lowest register address in virtual range.
+	range_min: u32,
+    /// Address of the highest register in virtual range.
+	range_max: u32,
+    /// Register with selector field.
+	selector_reg: u32,
+    /// Bit mask for selector value.
+	selector_mask: u32,
+    /// Bit shift for selector value.
+	selector_shift: i32,
+    /// Address of first (lowest) register in data window.
+	window_start: u32,
+    /// Number of registers in data window.
+	window_len: u32,
+}
+
 pub struct RegmapConfig {
     ///  Optional name of the regmap. Useful when a device has multiple
     /// register regions.
     name: Option<&'static CStr>,
     reg_bits: ,
+    ranges: &[RegmapRangeConfig],
 
 }
 
@@ -45,12 +84,12 @@ impl Into<bindings::regmap_config> for Regmap {
             reg_base: (),
             pad_bits: (),
             val_bits: (),
-            writeable_reg: (),
-            readable_reg: (),
-            volatile_reg: (),
-            precious_reg: (),
-            writeable_noinc_reg: (),
-            readable_noinc_reg: (),
+            writeable_reg: None,
+            readable_reg: None,
+            volatile_reg: None,
+            precious_reg: None,
+            writeable_noinc_reg: None,
+            readable_noinc_reg: None,
             disable_locking: (),
             lock: (),
             unlock: (),
